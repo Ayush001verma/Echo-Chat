d2b55f3bcee1187549e749c5bb89949ea8eaf120
Ayush Verma <ayush838207@gmail.com>
2026-04-23 14:19:34 +0530

    plumbing the routes

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage, deleteMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts",getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+router.delete("/:id", deleteMessage);
+
+export default router;
